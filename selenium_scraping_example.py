53c7898eae7c93788111bcb116ff58fc870587ba
Gisela <giselafloresb@gmail.com>
2023-12-07 16:07:00 -0800

    ACTUALIZACION

--- selenium_scraping_example.py
+++ selenium_scraping_example.py
@@ -1,77 +1,171 @@
 # Librerías
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 import time
 import pandas as pd
 
-# Opciones de navegación
+#ruta del ejecutable de ChromeDriver
+driver_path = 'C:\\Users\\gisel\\Desktop\\chromedriver-win64\\chromedriver.exe'
+
+# Opciones del navegación
 options = webdriver.ChromeOptions()
 options.add_argument('--start-maximized')
-options.add_argument('--disable-extensions')
-options.binary_location = 'C:\\chrome-win64\\chrome.exe'
+#options.add_argument('--disable-extensions')
 
-driver_path = 'C:\\chrome-win64\\chromedriver_win64\\chromedriver.exe'
+#Iniciar el navegador
+options.binary_location = 'C:\\Users\\gisel\\Desktop\\chrome-win64\\chrome.exe'
 
 # Aquí se corrige la forma en que se pasan las opciones al WebDriver
 driver = webdriver.Chrome(options=options)
 
-# Iniciar el navegador en una posición determinada (opcional)
-driver.set_window_position(2000, 0)
-driver.maximize_window()
-time.sleep(1)
+# Leer datos desde el archivo Excel
+#excel_path = 'C:\\Users\\gisel\\Documents\\robot_pyhton\\CAPTURA MIERCOLES 06 DIC.xlsx'
+#df = pd.read_excel(excel_path)
 
-# Inicializamos el navegador en la página deseada
-driver.get('https://www.gmail.com')
+# Iniciar el navegador en una posición determinada
+try:
+    # Ingresar a la página
+    driver.get('https://info.sirena.mx/index.php')
 
-user = WebDriverWait(driver, 5)\
-    .until(EC.element_to_be_clickable((By.CSS_SELECTOR,
-                                      'input#identifierId')))\
-    .send_keys('gallo.gallegos')
+    # Esperar hasta que el elemento sea clickeable
+    username = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.CSS_SELECTOR, "input[name='login']"))
+    )
+    username.send_keys('bezaadttvy')
 
-user = WebDriverWait(driver, 5)\
-    .until(EC.element_to_be_clickable((By.CSS_SELECTOR,
-                                      'input#identifierId')))\
-    .send_keys('gallo.gallegos')
-# WebDriverWait(driver, 5)\
-#     .until(EC.element_to_be_clickable((By.CSS_SELECTOR,
-#                                       'i.icon.icon-search')))\
-#     .click()
+    password = driver.find_element(By.CSS_SELECTOR, "input[name='password']")
+    password.send_keys('MQ8kpmbrgj')
 
-# WebDriverWait(driver, 5)\
-#     .until(EC.element_to_be_clickable((By.CSS_SELECTOR,
-#                                       'i.icon_weather_s.icon.icon-local')))\
-#     .click()
+    driver.find_element('id', 'go').click()
 
-# WebDriverWait(driver, 5)\
-#     .until(EC.element_to_be_clickable((By.XPATH,
-#                                       '/html/body/div[7]/main/div[4]/div/section[4]/section/div/article/section/ul/li[2]/a')))\
-#     .click()
+    # Seleccionar botones
+    promovidos_button = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.XPATH, "//button[contains(@onclick, 'window.location=\"./simpatiza.php\"')]"))
+    )
+    promovidos_button.click()
+
+    registrar_en_linea_button = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.CSS_SELECTOR, "button.btns.btns-ok span.ii i.fas.fa-link"))
+    )
+    registrar_en_linea_button.click()
+
+    # Cambiar a la nueva ventana o pestaña (si es necesario)
+    driver.switch_to.window(driver.window_handles[-1])
+
+    # Esperar hasta que el radio button sea clickeable
+    simpatizante_radio_button = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.ID, 'xxsimpatizante1'))
+    )
+    simpatizante_radio_button.click()
+
+    # Verificar si es simpatizante
+    if simpatizante_radio_button:        
+
+        # Seleccionar la opción para "medad"
+        medad_option = driver.find_element(By.CSS_SELECTOR, "input[name='medad']")
+        medad_option.click()
+
+        # Ingresar información en los campos
+        nombre_input = driver.find_element(By.ID, 'nombre')
+        nombre_input.send_keys('ANTONIO')
+        
+        time.sleep(1)
+        apellidop_input = driver.find_element(By.ID, 'paterno')
+        apellidop_input.send_keys('LOPEZ')
+
+        time.sleep(1)
+        apellidom_input = driver.find_element(By.ID, 'materno')
+        apellidom_input.send_keys('REINA')
+
+        time.sleep(1)
+        calle_input = driver.find_element(By.ID, 'calle')
+        calle_input.send_keys('AVENIDA VERDUGO')
+
+        time.sleep(1)
+        ext_input = driver.find_element(By.ID, 'num_ext')
+        ext_input.send_keys('47')
+
+        time.sleep(1)
+        municipio_dropdown = driver.find_element(By.ID, 'municipio')
+        municipio_dropdown.send_keys('Mexicali')
+
+        time.sleep(1)
+        colonia_input = driver.find_element(By.ID, 'colonia')
+        colonia_input.send_keys('CIUDAD COAHUILA')
+
+        time.sleep(1)
+        seccion_dropdown = driver.find_element(By.ID, 'seccion')
+        seccion_dropdown.send_keys('644')
+
+        time.sleep(1)
+        tel_input = driver.find_element(By.ID, 'tel')
+        tel_input.send_keys('6861993643')
+        
+        # Hacer clic en cualquier parte de la página
+        driver.find_element('id','info').click()
+
+        # Hacer clic en el botón "Enviar/Guardar"
+        enviar_guardar_button = WebDriverWait(driver, 100).until(
+            EC.element_to_be_clickable((By.ID, 'submitokm'))
+        )
+        enviar_guardar_button.click()
+
+        #Seleccionar por direccion
+        por_dir = WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.ID, 'apd'))
+        )
+        por_dir.click()
+
+        # Cambiar a la nueva ventana o pestaña
+        driver.switch_to.window(driver.window_handles[-1])
+
+        # Esperar 5 segundos después de abrir la nueva página
+        time.sleep(5)
+
+        # Hacer clic en el botón "Actualizar coordenadas"
+        actualizar_coordenadas_button = WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.ID, 'actualizar'))
+        )
+        actualizar_coordenadas_button.click()
+
+        # Esperar a que el botón "Cerrar" sea clickeable
+        time.sleep(2)
+        cerrar_button = WebDriverWait(driver, 5).until(
+            EC.element_to_be_clickable((By.CSS_SELECTOR, "button.btns.btns-gris[onclick='window.close();']"))
+        )
+        cerrar_button.click()
 
 
-# WebDriverWait(driver, 5)\
-#     .until(EC.element_to_be_clickable((By.XPATH,
-#                                       '/html/body/div[7]/main/div[4]/div/section[4]/section/div[1]/ul')))
+    else:
+        # Ingresar información si no es simpatizante
+        driver.find_element('input[name="calle"]').send_keys('NombreCalle')
+        driver.find_element('input[name="num_ext"]').send_keys('NumeroExterior')
+        driver.find_element('input[name="municipio"]').send_keys('Mexicali')
+        driver.find_element('input[name="colonia"]').send_keys('NombreColonia')
+        driver.find_element('input[name="seccion"]').send_keys('Seccion')
 
-# texto_columnas = driver.find_element_by_xpath(
-#     '/html/body/div[7]/main/div[4]/div/section[4]/section/div[1]/ul')
-# texto_columnas = texto_columnas.text
+        # Hacer clic en cualquier parte de la página (puedes cambiar el selector según tu necesidad)
+        driver.find_element('body').click()
 
-# tiempo_hoy = texto_columnas.split('Mañana')[0].split('\n')[1:-1]
+        # Seleccionar botones adicionales
+        driver.find_element('input[name="button"][id="submitokm"]').click()
+        driver.find_element('input[name="buton"][id="apd"]').click()
 
-# horas = list()
-# temp = list()
-# v_viento = list()
+        # Abrir nueva pestaña
+        driver.execute_script("window.open('https://2024.sirena.mx/upcoor.php', '_blank')")
 
-# for i in range(0, len(tiempo_hoy), 4):
-#     horas.append(tiempo_hoy[i])
-#     temp.append(tiempo_hoy[i+1])
-#     v_viento.append(tiempo_hoy[i+2])
+        # Esperar 3 segundos
+        time.sleep(3)
 
-# df = pd.DataFrame({'Horas': horas, 'Temperatura': temp,
-#                   'V_viento(km_h)': v_viento})
-# print(df)
-# df.to_csv('tiempo_hoy.csv', index=False)
-time.sleep(10)
-driver.quit()
+        # Actualizar y cerrar la nueva pestaña
+        driver.switch_to.window(driver.window_handles[1])
+        driver.find_element('input[name="button"][id="actualizar"]').click()
+        time.sleep(1)
+        driver.find_element('input[name="button"][id="window.close()"]').click()
+
+finally:
+    time.sleep(50)
+    driver.quit()
