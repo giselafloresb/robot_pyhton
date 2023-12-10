77d49ddc23c5959857cd106e878a99f89a85c986
Gisela Flores <giselafloresb@gmail.com>
2023-12-10 13:29:16 -0800

    Cambios de 10/12/23

--- selenium_scraping_example.py
+++ selenium_scraping_example.py
@@ -15,6 +15,9 @@ driver_path = 'C:\\Users\\gisel\\Desktop\\chromedriver-win64\\chromedriver.exe'
 
 # Ruta del archivo Excel
 excel_path = 'C:\\Users\\gisel\\Documents\\robot_pyhton\\CAPTURA VIERNES 08 DIC.xlsx'
+workbook = load_workbook(excel_path)
+sheet = workbook.active
+fila = 2
 
 # Opciones del navegación
 options = webdriver.ChromeOptions()
@@ -67,89 +70,94 @@ try:
         sheet = workbook.active  
 
         # Obtener datos del Excel
-        nombre = sheet['A2'].value
-        apellidop = sheet['B2'].value
-        apellidom = sheet['C2'].value
-        #cve_ife = sheet[].value
-        calle = sheet['D2'].value
-        ext = sheet['E2'].value  
-        municipio = sheet['F2'].value
-        colonia = sheet['G2'].value
-        seccion = sheet['H2'].value
-        telefono = sheet['I2'].value
+        while True:
+            nombre = sheet[f'A{fila}'].value
+            apellidop = sheet[f'B{fila}'].value
+            apellidom = sheet[f'C{fila}'].value
+            calle = sheet[f'D{fila}'].value
+            ext = sheet[f'E{fila}'].value  
+            municipio = sheet[f'F{fila}'].value
+            colonia = sheet[f'G{fila}'].value
+            seccion = sheet[f'H{fila}'].value
+            telefono = sheet[f'I{fila}'].value
 
-        # Seleccionar la opción para "medad"
-        medad_option = driver.find_element(By.ID, 'xxmedad1')
-        medad_option.click()
+            # Seleccionar la opción para "medad"
+            medad_option = driver.find_element(By.ID, 'xxmedad1')
+            medad_option.click()
 
-        # Ingresar información en los campos
-        nombre_input = driver.find_element(By.ID, 'nombre')
-        nombre_input.send_keys(nombre)
-        
-        time.sleep(1)
-        apellidop_input = driver.find_element(By.ID, 'paterno')
-        apellidop_input.send_keys(apellidop)
+            if nombre is None:
+                break
 
-        time.sleep(1)
-        apellidom_input = driver.find_element(By.ID, 'materno')
-        apellidom_input.send_keys(apellidom)
+            # Ingresar información en los campos
+            nombre_input = driver.find_element(By.ID, 'nombre')
+            nombre_input.send_keys(nombre)
+            
+            time.sleep(1)
+            apellidop_input = driver.find_element(By.ID, 'paterno')
+            apellidop_input.send_keys(apellidop)
 
-        time.sleep(1)
-        calle_input = driver.find_element(By.ID, 'calle')
-        calle_input.send_keys(calle)
+            time.sleep(1)
+            apellidom_input = driver.find_element(By.ID, 'materno')
+            apellidom_input.send_keys(apellidom)
 
-        time.sleep(1)
-        ext_input = driver.find_element(By.ID, 'num_ext')
-        ext_input.send_keys(ext)
+            time.sleep(1)
+            calle_input = driver.find_element(By.ID, 'calle')
+            calle_input.send_keys(calle)
 
-        time.sleep(1)
-        municipio_dropdown = driver.find_element(By.ID, 'municipio')
-        municipio_dropdown.send_keys(municipio)
+            time.sleep(1)
+            ext_input = driver.find_element(By.ID, 'num_ext')
+            ext_input.send_keys(ext)
 
-        time.sleep(1)
-        colonia_input = driver.find_element(By.ID, 'colonia')
-        colonia_input.send_keys(colonia)
+            time.sleep(1)
+            municipio_dropdown = driver.find_element(By.ID, 'municipio')
+            municipio_dropdown.send_keys(municipio)
 
-        time.sleep(1)
-        seccion_dropdown = driver.find_element(By.ID, 'seccion')
-        seccion_dropdown.send_keys(seccion)
+            time.sleep(1)
+            colonia_input = driver.find_element(By.ID, 'colonia')
+            colonia_input.send_keys(colonia)
 
-        # Verificar si el valor ingresado está presente en el campo
-        # try:
-        #     # Esperar hasta que el elemento con el valor ingresado aparezca
-        #     WebDriverWait(driver, 2).until(
-        #         EC.text_to_be_present_in_element((By.XPATH, '//*[@id="seccion"]'), seccion)
-        #     )
+            time.sleep(1)
+            seccion_dropdown = driver.find_element(By.ID, 'seccion')
+            seccion_dropdown.send_keys(seccion)
 
-            # Si encuentra el valor, seguir con el resto del código...
-        tel_input = driver.find_element(By.ID, 'tel')
-        tel_input.send_keys(telefono)
+            # Verificar si el valor ingresado está presente en el campo
+            # try:
+            #     # Esperar hasta que el elemento con el valor ingresado aparezca
+            #     WebDriverWait(driver, 2).until(
+            #         EC.text_to_be_present_in_element((By.XPATH, '//*[@id="seccion"]'), seccion)
+            #     )
 
-            # Hacer clic en cualquier parte de la página
-        driver.find_element('id','info').click()
+                # Si encuentra el valor, seguir con el resto del código...
+            tel_input = driver.find_element(By.ID, 'tel')
+            tel_input.send_keys(telefono)
 
-        # Hacer clic en el botón "Enviar/Guardar"
-        enviar_guardar_button = WebDriverWait(driver, 100).until(
-            EC.element_to_be_clickable((By.ID, 'submitokm'))
-        )
-        enviar_guardar_button.click()
+            fila += 1
 
-        # Esperar a que el mensaje de error aparezca (max 5 segundos)
-        error_message = WebDriverWait(driver, 5).until(
-            EC.presence_of_element_located((By.XPATH, "//span[contains(text(),'ERROR:')]"))
-        )
+                # Hacer clic en cualquier parte de la página
+            driver.find_element('id','info').click()
 
-        # Si el mensaje de error está presente, hacer clic en el botón "Volver"
-        volver_button = WebDriverWait(driver, 10).until(
-            EC.element_to_be_clickable((By.XPATH, "//button[contains(@onclick, 'window.location=\"./sload.php\"')]"))
-        )
-        volver_button.click()
+            # Hacer clic en el botón "Enviar/Guardar"
+            enviar_guardar_button = WebDriverWait(driver, 100).until(
+                EC.element_to_be_clickable((By.ID, 'submitokm'))
+            )
+            enviar_guardar_button.click()
 
-        #Seleccionar por direccion
-        por_dir = WebDriverWait(driver, 10).until(
-            EC.element_to_be_clickable((By.ID, 'apd'))
-        )
-        por_dir.click()
+            # Esperar a que el mensaje de error aparezca (max 5 segundos)
+            error_message = WebDriverWait(driver, 5).until(
+                EC.presence_of_element_located((By.XPATH, "//span[contains(text(),'ERROR:')]"))
+            )
+
+            # Si el mensaje de error está presente, hacer clic en el botón "Volver"
+            volver_button = WebDriverWait(driver, 10).until(
+                EC.element_to_be_clickable((By.XPATH, "//button[contains(@onclick, 'window.location=\"./sload.php\"')]"))
+            )
+            volver_button.click()
+
+            #Seleccionar por direccion
+            por_dir = WebDriverWait(driver, 10).until(
+                EC.element_to_be_clickable((By.ID, 'apd'))
+            )
+            por_dir.click()
 
         # Cambiar a la nueva ventana o pestaña
         driver.switch_to.window(driver.window_handles[-1])
@@ -182,5 +190,5 @@ except TimeoutException:
     pass
 
 finally:
-    time.sleep(50)
+    time.sleep(10)
     driver.quit()
